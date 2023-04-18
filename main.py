c473922b88a17ac923ada6102c58e1fefa702847
dhesru <surendhars@hotmail.com>
2023-04-18 18:19:44 +0800

    amend main app

--- main.py
+++ main.py
@@ -137,10 +137,10 @@ def detect_anomalies(scale,pca,n_comp,fe):
             plt.clf()
     st.dataframe(df_res)
 
-st.write('This anomaly detector derives condition indicators such as Standard Deviation, Mean,Kurtosis, Variance and Max using a rolling window of size 5. '
-         'The derived features are then condensed into 5 components using Principal Component Analysis'
-         'These features are the fed into Isolation forest for anomalous points to be predicted'
-         'To begin detecting anomalous points in your multivariate time series, proceed to upload.'
+st.write('This anomaly detector derives condition indicators such as Standard Deviation, Mean, Kurtosis, Variance and Max using a rolling window of size 5. '
+         'The derived features are then condensed into 5 components using Principal Component Analysis.'
+         ' These features are the fed into Isolation forest for anomalous points to be predicted.'
+         ' To begin detecting anomalous points in your multivariate time series, proceed to upload.'
          )
 
 if uploaded_file is not None:
